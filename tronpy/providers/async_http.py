412c5f123785a3a2ac6e3d54647b2a4d7b79a84b
NitaKai <32329170+NitaKai@users.noreply.github.com>
2024-11-01 17:04:10 +0300

    [+] Multi client for async tron

--- tronpy/providers/async_http.py
+++ tronpy/providers/async_http.py
@@ -1,14 +1,24 @@
 import os
-from typing import Any, Union
+from typing import Any, List, Union
 from urllib.parse import urljoin
-
+import sys
+import asyncio
 import httpx
 from httpx import Timeout
 
+import random
 from tronpy.version import VERSION
 
 DEFAULT_TIMEOUT = 10.0
 DEFAULT_API_KEY = "f92221d5-7056-4366-b96f-65d3662ec2d9"
+DEFAULT_API_KEYS = [
+    "f92221d5-7056-4366-b96f-65d3662ec2d9",
+    "1e0a625f-cfa5-43ee-ba41-a09db1aae55f",
+    "f399168e-2259-481c-90fc-6b3d984c5463",
+    "da63253b-aa9c-46e7-a4e8-22d259a8026d",
+    "88c10958-af7b-4d5a-8eef-6e84bf5fb809",
+    "169bb4b3-cbe8-449a-984e-80e9adacac55",
+]
 
 
 class AsyncHTTPProvider:
@@ -24,7 +34,7 @@ class AsyncHTTPProvider:
         endpoint_uri: Union[str, dict] = None,
         timeout: float = DEFAULT_TIMEOUT,
         client: httpx.AsyncClient = None,
-        api_key: str = DEFAULT_API_KEY,
+        api_key: Union[str, List[str]] = DEFAULT_API_KEY,
         jw_token: str = None,
     ):
         super().__init__()
@@ -38,11 +48,22 @@ class AsyncHTTPProvider:
         else:
             raise TypeError(f"unknown endpoint uri {endpoint_uri}")
 
-        headers = {"User-Agent": f"Tronpy/{VERSION}", "Tron-Pro-Api-Key": api_key}
-        if jw_token is not None:
-            headers["Authorization"] = f"Bearer {jw_token}"
+        if "trongrid" in self.endpoint_uri:
+            self.use_api_key = True
+            if isinstance(api_key, (str,)):
+                self._api_keys = [api_key]
+            elif isinstance(api_key, (list,)) and api_key:
+                self._api_keys = api_key
+            else:
+                self._api_keys = DEFAULT_API_KEYS.copy()
+
+            self._default_api_keys = self._api_keys.copy()
+        else:
+            self.use_api_key = False
+        self.jw_token = jw_token
+
         if client is None:
-            self.client = httpx.AsyncClient(headers=headers, timeout=Timeout(timeout))
+            self.client = httpx.AsyncClient(timeout=Timeout(timeout))
         else:
             self.client = client
 
@@ -50,9 +71,35 @@ class AsyncHTTPProvider:
         """Request timeout in second."""
 
     async def make_request(self, method: str, params: Any = None) -> dict:
+        headers = {}
+        if self.use_api_key:
+            headers["Tron-Pro-Api-Key"] = self.random_api_key
+            headers["User-Agent"] = f"Tronpy/{VERSION}"
+
+        if self.jw_token is not None:
+            headers["Authorization"] = f"Bearer {self.jw_token}"
+
         if params is None:
             params = {}
         url = urljoin(self.endpoint_uri, method)
-        resp = await self.client.post(url, json=params)
+        resp = await self.client.post(headers=headers, url=url, json=params)
+
+        if self.use_api_key:
+            if resp.status_code == 403 and b"Exceed the user daily usage" in resp.content:
+                print("W:", resp.json().get("Error", "rate limit!"), file=sys.stderr)
+                await self._handle_rate_limit(headers["Tron-Pro-Api-Key"])
+                return await self.make_request(method, params)
+
         resp.raise_for_status()
         return resp.json()
+
+    @property
+    def random_api_key(self):
+        return random.choice(self._api_keys)
+
+    async def _handle_rate_limit(self, used_key):
+        if len(self._api_keys) > 1:
+            self._api_keys.remove(used_key)
+        else:
+            print("W: Please add as-many API-Keys in HTTPProvider", file=sys.stderr)
+            await asyncio.sleep(0.9)
